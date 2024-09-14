6dce6c3834ef42228eda230f0daab905a75ce54b
saneeya sandeep Vichare <saneeya@scc-wl2.scc.bu.edu>
2024-09-14 14:22:29 -0400

    All tests passed!

--- main.R
+++ main.R
@@ -1,5 +1,40 @@
 # ----------------------- Helper Functions to Implement ------------------------
 
+
+# ------------ Helper Functions Used By Assignment, You May Ignore ------------
+sample_normal <- function(n, mean=0, sd=1) {
+  set.seed(1337)
+  samples <- rnorm(n, mean=mean, sd=sd)
+  return(samples)
+}
+
+sample_normal_w_missing <- function(n, mean=0, sd=1, missing_frac=0.1) {
+  set.seed(1337)
+  samples <- rnorm(n, mean=mean, sd=sd)
+  missing <- rbinom(length(samples), 1, missing_frac)==1
+  samples[missing] <- NA
+  return(samples)
+}
+
+simulate_gene_expression <- function(num_samples, num_genes) {
+  set.seed(1337)
+  gene_exp <- matrix(
+    rnbinom(num_samples*num_genes, rlnorm(num_genes,meanlog = 3), prob=runif(num_genes)),
+    nrow=num_genes
+  )
+  return(gene_exp)
+}
+
+simulate_gene_expression_w_missing <- function(num_samples, num_genes, missing_frac=0.1) {
+  gene_exp <- simulate_gene_expression(num_samples, num_genes)
+  missing <- matrix(
+    rbinom(num_samples*num_genes, 1, missing_frac)==1,
+    nrow=num_genes
+  )
+  gene_exp[missing] <- NA
+  return(gene_exp)
+}
+
 #' Evaluate whether the argument is less than 2
 #'
 #' Returns TRUE if the numeric argument x is a prime number, otherwise returns
@@ -18,8 +53,9 @@
 #' less_than_zero(c(-1,0,1,2,3,4))
 #' [1] TRUE FALSE FALSE FALSE FALSE FALSE
 less_than_zero <- function(x) {
-    return(NULL)
+    return(x<0)
 }
+
 
 #' Evaluate whether the argument is between two numbers
 #'
@@ -44,8 +80,9 @@ less_than_zero <- function(x) {
 #' [2,]  TRUE FALSE FALSE
 #' [3,] FALSE FALSE FALSE
 is_between <- function(x, a, b) {
-    return(NULL)
+    return(x>a & x<b)
 }
+
 
 #' Return the values of the input vector that are not NA
 #'
@@ -61,8 +98,10 @@ is_between <- function(x, a, b) {
 #' rm_na(x)
 #' [1] 1 2 3
 rm_na <- function(x) {
-    return(NULL)
+  return(x[!is.na(x)])
 }
+# in this function, the return block will evaluate if the value of x is not (through!) equal to NA
+
 
 #' Calculate the median of each row of a matrix
 #'
@@ -80,8 +119,9 @@ rm_na <- function(x) {
 #' [1] 1 4 7
 #' 
 row_medians <- function(x) {
-    return(NULL)
+  apply(x, 1, median)
 }
+
 
 #' Evaluate each row of a matrix with a provided function
 #'
@@ -105,8 +145,9 @@ row_medians <- function(x) {
 #' summarize_rows(m, mean)
 #' [1] 2 5 8
 summarize_rows <- function(x, fn, na.rm=FALSE) {
-    return(NULL)
+  apply(x, 1, function(row) fn(row, na.rm = na.rm))
 }
+
 
 #' Summarize matrix rows into data frame
 #'
@@ -144,23 +185,35 @@ summarize_rows <- function(x, fn, na.rm=FALSE) {
 #' 2 -0.01574033 1.026951 -0.04725656 -2.967057 2.571608      112              70      0
 #' 3 -0.09040182 1.027559 -0.02774705 -3.026888 2.353087      130              54      0
 #' 4  0.09518138 1.030461  0.11294781 -3.409049 2.544992       90              72      0
+
+
 summarize_matrix <- function(x, na.rm=FALSE) {
-    return(NULL)
+  # single function to perform all the analyses
+  summarize_row <- function(row, na.rm) {
+    if (na.rm) {
+      row <- na.omit(row)
+    }
+    
+    mean_value <- mean(row, na.rm = na.rm)
+    stdev_value <- sd(row, na.rm = na.rm)
+    median_value <- median(row, na.rm = na.rm)
+    min_value <- min(row, na.rm = na.rm)
+    max_value <- max(row, na.rm = na.rm)
+    num_lt_0 <- sum(row < 0, na.rm = na.rm)
+    num_btw_1_and_5 <- sum(row > 1 & row < 5, na.rm = na.rm)
+    num_na <- sum(is.na(row))
+    
+    return(c(mean = mean_value, stdev = stdev_value, median = median_value,
+             min = min_value, max = max_value, num_lt_0 = num_lt_0,
+             num_btw_1_and_5 = num_btw_1_and_5, num_na = num_na))
+  }
+  
+  # Apply the summarize_row function to each row of the matrix
+  row_summaries <- t(apply(x, 1, summarize_row, na.rm = na.rm))
+  
+  # values added to a dataframe
+  result_df <- as.data.frame(row_summaries)
+  
+  return(result_df)
 }
 
-# ------------ Helper Functions Used By Assignment, You May Ignore ------------
-sample_normal <- function(n, mean=0, sd=1) {
-    return(NULL)
-}
-
-sample_normal_w_missing <- function(n, mean=0, sd=1, missing_frac=0.1) {
-    return(NULL)
-}
-
-simulate_gene_expression <- function(num_samples, num_genes) {
-    return(NULL)
-}
-
-simulate_gene_expression_w_missing <- function(num_samples, num_genes, missing_frac=0.1) {
-    return(NULL)
-}
